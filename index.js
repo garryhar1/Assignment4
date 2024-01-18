12e700813608eba884cb9b7e82b2d8c7cd3e0cf5
Garryhar1 <hardiantogarry82@gmail.com>
2024-01-18 15:38:49 +0700

    tambah

---
+++ index.js
@@ -0,0 +1,39 @@
+const data1 = document.getElementById('data1')
+const data2 = document.getElementById('data2')
+const data3 = document.getElementById('data3')
+const data4 = document.getElementById('data4')
+const data5 = document.getElementById('data5')
+const data6 = document.getElementById('data6')
+
+
+const url = 'https://covid-193.p.rapidapi.com/statistics';
+ const options = {
+    method: 'GET',
+   headers: {
+       'X-RapidAPI-Key': '4bdb2f0a49msh645dc5c5c1644adp1d970bjsn66e93ae62258',
+        'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+    }
+};
+
+
+let datafetch = async () => {
+    const con = document.getElementById('country').value.trim()
+
+    try {
+        const response = await fetch(`https://covid-193.p.rapidapi.com/statistics?country=${con}`, options);
+    
+        if (!response.ok) {
+            throw new Error(`Getting error: ${response}`);
+        }
+    
+        const data = await response.json();
+        data1.innerHTML = data.response[0].cases.active ?? 0
+        data2.innerHTML = data.response[0].cases.new ?? 0
+        data3.innerHTML = data.response[0].cases.recovered ?? 0
+        data4.innerHTML = data.response[0].cases.total ?? 0
+        data5.innerHTML = data.response[0].deaths.total ?? 0
+        data6.innerHTML = data.response[0].tests.total ?? 0
+      } catch (error) {
+          console.log(error);
+      }
+}
